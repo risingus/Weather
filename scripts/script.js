02eeab603c7c5b94c70de702b09fbfd8e2b1665d
Gustavo Lima <gustavo.it@outlook.com>
2020-10-27 20:40:45 -0300

    1.selecionado apenas um background da página, independente de clima. 2. Removido comentários do código js. 3. ajustado cores, tamanhos da página.

--- scripts/script.js
+++ scripts/script.js
@@ -9,40 +9,10 @@ function searchWeather(searchTerm) {
         return result.json();
     }).then(result => {
         init(result);
-    })  
+    })
 };
 
 function init(resultFromServer) {
-    // switch (resultFromServer.weather[0].main) {
-    //     case 'Clear':
-    //         document.body.style.backgroundImage = 'url("./assets/img/clear.jpg")';
-    //         break;
-
-    //     case 'Clouds':
-    //         document.body.style.backgroundImage = 'url("./assets/img/cloudy.jpg")';
-    //         break;
-
-    //     case 'Rain':
-    //     case 'Drizzle':
-    //         document.body.style.backgroundImage = 'url("./assets/img/rain.jpg")';
-    //         break;
-
-    //     case 'Mist':
-    //         document.body.style.backgroundImage = 'url("./assets/img/misty.jpg")';
-    //         break;
-
-    //     case 'Thunderstorm':
-    //         document.body.style.backgroundImage = 'url("./assets/img/storm.jpg")';
-    //         break;
-
-    //     case 'Snow':
-    //         document.body.style.backgroundImage = 'url("./assets/img/snow.jpg")';
-    //         break;
-
-    //     default:
-    //         document.body.style.backgroundImage = 'url("./assets/img/default.jpg")';
-    //         break;
-    // }
 
     let weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
     let temperatureElement = document.getElementById('temperature');
@@ -57,24 +27,23 @@ function init(resultFromServer) {
     weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
 
     temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176  C';
-    windSpeedElement.innerHTML = 'Velocidade do vento: ' + Math.floor(resultFromServer.wind.speed) + ' m/s'; 
+    windSpeedElement.innerHTML = 'Velocidade do vento: ' + Math.floor(resultFromServer.wind.speed) + ' m/s';
     cityHeader.innerHTML = resultFromServer.name;
     humidityElement.innerHTML = 'Humidade: ' + resultFromServer.main.humidity + '%';
 
     showWeatherInfo();
 
- }
+}
 
- function showWeatherInfo(){
+function showWeatherInfo() {
     let weatherContainer = document.getElementById('weatherContainer');
     weatherContainer.style.visibility = 'visible';
 
- }
-
+}
 
 document.getElementById('searchBtn').addEventListener('click', () => {
     let searchTerm = document.getElementById('searchInput').value;
 
-    if(searchTerm)
+    if (searchTerm)
         searchWeather(searchTerm);
 })
