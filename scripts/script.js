65478d4b6ce7095ee35715098158bf1464e8fed7
Gustavo Lima <gustavo.it@outlook.com>
2020-10-28 12:17:04 -0300

    1. adicionado temperatura max e min. 2. Adicionado país. 3. Trocado humidade para sensação térmica. 4. Modificado transparêcia de painel de resultados e input de pesquisa. 5. Mudado texto do placeholder. 6. Mudado texto do botão de busca. 6. Mudado textos para inglês, incluído resultado do clima.

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 let appID = 'a324b7f2eafab98f6acaf474fbed1b4d';
 let units = 'metric';
-let lang = 'pt_br';
+let lang = 'en_us';
 
 
 function searchWeather(searchTerm) {
@@ -16,8 +16,10 @@ function init(resultFromServer) {
 
     let weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
     let temperatureElement = document.getElementById('temperature');
+    let temp_minElement = document.getElementById('temp_min');
+    let temp_maxElement = document.getElementById('temp_max');
     let windSpeedElement = document.getElementById('windSpeed');
-    let humidityElement = document.getElementById('humidity');
+    let feels_likeElement = document.getElementById('feels_like');
     let cityHeader = document.getElementById('cityHeader');
     let weatherIcon = document.getElementById('documentIconImg');
 
@@ -27,9 +29,13 @@ function init(resultFromServer) {
     weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
 
     temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176  C';
-    windSpeedElement.innerHTML = 'Velocidade do vento: ' + Math.floor(resultFromServer.wind.speed) + ' m/s';
-    cityHeader.innerHTML = resultFromServer.name;
-    humidityElement.innerHTML = 'Humidade: ' + resultFromServer.main.humidity + '%';
+    temp_minElement.innerHTML = 'Min: ' +  Math.floor(resultFromServer.main.temp_min) + '&#176  C';
+    temp_maxElement.innerHTML = 'Max: ' + Math.floor(resultFromServer.main.temp_max) + '&#176  C';
+    windSpeedElement.innerHTML = 'Winds: ' + Math.floor(resultFromServer.wind.speed) + ' m/s';
+    cityHeader.innerHTML = resultFromServer.name + ', ' +  resultFromServer.sys.country;
+    feels_likeElement.innerHTML = 'Thermal Sensation: ' + resultFromServer.main.feels_like + '&#176  C';
+
+    console.log(resultFromServer);
 
     showWeatherInfo();
 
@@ -48,3 +54,4 @@ document.getElementById('searchBtn').addEventListener('click', () => {
     if (searchTerm)
         searchWeather(searchTerm);
 })
+
